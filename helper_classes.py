04ae783d5edf6136a9d754dae21a4be4519cef8f
The6thChampion <The6thChampion1883@protonmail.com>
2022-02-22 19:48:10 -0800

    map is added, as well as move command

--- helper_classes.py
+++ helper_classes.py
@@ -1,4 +1,6 @@
 from typing import List, Optional, Tuple, Dict
+from global_vars import items
+
 
 class PlayerData:
     def __init__(self, load_data: Dict) -> None:
@@ -9,28 +11,49 @@ class PlayerData:
         self.achievements: Achievements = Achievements(
             achievement_data=load_data["achievements"])
         self.gameover = load_data["gameover"]
+        self.area_map: AreaMap = AreaMap(
+            map_data=load_data["area_map"], current_position=load_data["player"]["position"])
+
     def save(self) -> Dict:
         import json
+        self.player_info.position = self.area_map.player_cursor
         data = {
             "player": self.player_info.player_info,
             "inventory": self.inventory.inventory,
             "achievements": {
-                "all": self.achievements.all, 
+                "all": self.achievements.all,
                 "unlocked": self.achievements.unlocked
-                },
+            },
             "gameover": self.gameover,
             "newGame": False
-            }
+        }
         print(data)
         with open(".\playerData.json", "w") as fpdata:
             fpdata.write(json.dumps(data, indent=4))
         return f"Saved Successfully, {data['player']['name']}"
 
 
+class AreaMap:
+    def __init__(self, map_data, current_position) -> None:
+        self.area_map: List[List[str]] = map_data
+        self.player_cursor = current_position
+        self.area_map[self.player_cursor[0]][self.player_cursor[1]].append("player")
+        print("Added to map!")
+
+    def move_player(self, new_pos) -> None:
+        self.area_map[self.player_cursor[0]][self.player_cursor[1]].remove("player")
+        self.player_cursor = new_pos
+        self.area_map[self.player_cursor[0]][self.player_cursor[1]].append("player")
+
+    def reveal_items(self):
+        return self.area_map[self.player_cursor[0]][self.player_cursor[1]]
+
+
 class Achievements:
     def __init__(self, achievement_data) -> None:
         self.all = achievement_data["all"]
         self.unlocked = achievement_data["unlocked"]
+        print("Achievements synced!")
 
     def unlock(self, achievement) -> None:
         self.unlocked.append(achievement)
@@ -41,6 +64,7 @@ class Information:
         self.player_info = info_data
         self.name = info_data["name"]
         self.position = info_data["position"]
+        print("User Data synced!")
 
     def set_position(self, pos) -> None:
         self.position = pos
@@ -49,6 +73,7 @@ class Information:
 class Inventory:
     def __init__(self, inventory_data: Dict) -> None:
         self.inventory = inventory_data
+        print("Inventory Loaded!")
 
     def add_items(self, category: str, *, items: List[Tuple[str, int]]):
         for item in items:
@@ -56,7 +81,6 @@ class Inventory:
                 self.inventory[category][item[0]] = item[1]
             else:
                 self.inventory[category][item[0]] += item[1]
-                
 
     def view_items(self, category: Optional[str] = None) -> List[str]:
         output: str = ""
@@ -77,11 +101,41 @@ class Commands:
         pass
 
     @classmethod
+    def move(self, data: PlayerData, directions):
+        player_pos = data.area_map.player_cursor.copy()
+        if not directions:
+            return "Try that again, but with a direction (up, down, left, right)"
+        for direction in directions:
+            if direction == "up":
+                player_pos[0] -= 1
+                if (player_pos[0] <= 0):
+                    player_pos[0] = 0
+                    print("You cannot move farther up")
+            elif direction == "down":
+                data.player_info.position[0] += 1
+                if (data.player_info.position[0] >= 14):
+                    data.player_info.position[0] = 14
+                    print("You cannot move farther down")
+            elif direction == "left":
+                data.player_info.position[0] -= 1
+                if (data.player_info.position[0] <= 0):
+                    data.player_info.position[0] = 0
+                    print("You cannot move farther left")
+            elif direction == "right":
+                data.player_info.position[0] += 1
+                if (data.player_info.position[0] >= 14):
+                    data.player_info.position[0] = 14
+                    print("You cannot move farther right")
+        data.area_map.move_player(player_pos)
+        return "Surroundings: " + "".join(data.area_map.reveal_items())
+
+    @classmethod
     def save(self, data: PlayerData):
         data.save()
 
     @classmethod
     def stop(self, data: PlayerData):
+        self.area_map.area_map[self.player_info.position[0]][self.player_info.position[1]].remove("player")
         exit()
 
     @classmethod
@@ -92,11 +146,15 @@ class Commands:
         cat = input("Category >> ")
         output += data.inventory.view_items(cat)
         return output
-    
+
     @classmethod
     def grab(self, data: PlayerData, item: Tuple[str, int]):
-        data.inventory.add_items()
-    
+        data.inventory.add_items(items[item][0], item)
+
     @classmethod
     def help(self, data: PlayerData):
         print("Command list")
+
+    @classmethod
+    def show(self, data: PlayerData):
+        return ", ".join(data.area_map.reveal_items())
--- main.py
+++ main.py
@@ -2,38 +2,27 @@ import json
 from typing import List, Dict, Optional, Union, Tuple, Callable
 import os
 from helper_classes import *
+from global_vars import items
 
 commands: Dict[str, Callable] = {
     "save": Commands.save,
     "stop": Commands.stop,
     "inventory": Commands.inventory,
     "grab": Commands.grab,
-    "move": Commands.tbd,
+    "move": Commands.move,
     "help": Commands.help,
-}
-
-items: Dict[str, Tuple[str, str]] = {
-    "steal sword": ("weapon", "A relatively weak weapon, can be used to attack most animals"),
-    "hot sword": ("weapon", "A sword that's blade is hot enough to easily cauterize wounds, set fire to things, and cause a great deal of damage. It is made by forging steel sword and cinnabar together."),
-    "firewood": ("material", "to be used for heating up solutions"),
-    "spoon": ("material", "A very versitile utensil that is capable of being used for making Alkahest."),
-    "iron shavings": ("material", "Iron shavings... could be used to make iron sulfate"),
-    "sulfate": ("material", "A common item in an alchemists bag. Who knows what it can do..."),
-    "Vitriol of Mars": ("component", "Also known as Ferrous sulfate, this is a component in making Alkahest. A mole of this is made with one mole of aqueous iron and one mole of sulfate"),
-    "Cinnabar": ("component", "A solid substance used for making Alkahest. Found in Dragons' Lairs"),
-    "bezoar": ("component", "An item found within most animals, extract using a hot sword to keep the animal alive."),
-    "Dragon's Horn": ("component", "The horn of a dragon, and one of the only materials that can hold Alkahest. Must use a hot sword to cut it off."),
+    "show": Commands.show
 }
 
 
-def main(player_data: PlayerData) -> None:
+def game(player_data: PlayerData) -> None:
     # Game loop
     while True:
         prompt = input(">> ").strip().split(" ")
         if not prompt:
             continue
         if prompt[0] in commands and len(prompt) > 1:
-            print(commands[prompt[0]](player_data, **prompt[2:]))
+            print(commands[prompt[0]](player_data, prompt[1:]))
             pass
         elif prompt[0] in commands and len(prompt) == 1:
             print(commands[prompt[0]](data=player_data))
@@ -49,4 +38,4 @@ if __name__ == '__main__':
         raw_player_data["player"]["name"] = input("Enter player name >> ")
     print(f'Welcome to Alka-hassle, {raw_player_data["player"]["name"]}!')
     player_data: PlayerData = PlayerData(raw_player_data)
-    main(player_data)
+    game(player_data)
