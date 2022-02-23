552e062c69c2b73b06c709b50ac2a4ca6a6b8fb9
The6thChampion <The6thChampion1883@protonmail.com>
2022-02-22 19:47:41 -0800

    move items to a new file. move and show work

---
+++ global_vars.py
@@ -0,0 +1,15 @@
+from typing import Dict, Tuple
+
+items: Dict[str, Tuple[str, str]] = {
+    "steal sword": ("weapon", "A relatively weak weapon, can be used to attack most animals"),
+    "hot sword": ("weapon", "A sword that's blade is hot enough to easily cauterize wounds, set fire to things, and cause a great deal of damage. It is made by forging steel sword and cinnabar together."),
+    "firewood": ("material", "to be used for heating up solutions"),
+    "spoon": ("material", "A very versitile utensil that is capable of being used for making Alkahest."),
+    "iron shavings": ("material", "Iron shavings... could be used to make iron sulfate"),
+    "sulfate": ("material", "A common item in an alchemists bag. Who knows what it can do..."),
+    "Vitriol of Mars": ("component", "Also known as Ferrous sulfate, this is a component in making Alkahest. A mole of this is made with one mole of aqueous iron and one mole of sulfate"),
+    "Cinnabar": ("component", "A solid substance used for making Alkahest. Found in Dragons' Lairs"),
+    "bezoar": ("component", "An item found within most animals, extract using a hot sword to keep the animal alive."),
+    "Dragon's Horn": ("component", "The horn of a dragon, and one of the only materials that can hold Alkahest. Must use a hot sword to cut it off."),
+    "Alkahest": ("material", "The universal solvent. Congradulations, you have completed the game!")
+}
